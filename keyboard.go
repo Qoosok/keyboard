ece6f48cf4fc7740de01595b539b16ebbd592ec1
Qoosok <66019448+Qoosok@users.noreply.github.com>
2023-11-20 16:55:51 +0300

    Add files via upload

---
+++ keyboard.go
@@ -0,0 +1,23 @@
+package keyboard // добавляем директиву с именем пакета
+
+import (
+	"bufio" // импортируем только пакеты используемые в этом файле
+	"os"
+	"strconv"
+	"strings"
+)
+
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
